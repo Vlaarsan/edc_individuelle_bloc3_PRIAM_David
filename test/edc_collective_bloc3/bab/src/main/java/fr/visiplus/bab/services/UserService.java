f141d19b70d4de36ee91ed0998cc837a3c07b93b
Vlaarsan <priamdavidpro@gmail.com>
2026-04-15 10:19:29 +0200

    add user registration functionality

--- test/edc_collective_bloc3/bab/src/main/java/fr/visiplus/bab/services/UserService.java
+++ test/edc_collective_bloc3/bab/src/main/java/fr/visiplus/bab/services/UserService.java
@@ -1,10 +1,5 @@
 package fr.visiplus.bab.services;
 
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-
 import fr.visiplus.bab.dtos.BookDTO;
 import fr.visiplus.bab.dtos.LoginRequest;
 import fr.visiplus.bab.dtos.ReservationDTO;
@@ -12,41 +7,72 @@ import fr.visiplus.bab.dtos.UserDTO;
 import fr.visiplus.bab.entities.Reservation;
 import fr.visiplus.bab.entities.User;
 import fr.visiplus.bab.repositories.UserRepository;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-	private UserRepository userRepository;
+  private UserRepository userRepository;
 
-	public UserService(final UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+  public UserService(final UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
-	public Optional<UserDTO> login(final LoginRequest login) {
-		Optional<User> user = this.userRepository.findByUsernameAndPassword(login.getUsername(), login.getPassword());
-		UserDTO userDTO = null;
-		if (user.isPresent()) {
-			userDTO = convert(user.get());
-		}
-		return Optional.ofNullable(userDTO);
-	}
+  public UserDTO register(final User user) {
+    Optional<User> existingUser = userRepository.findByUsernameAndPassword(
+      user.getUsername(),
+      user.getPassword()
+    );
 
-	private UserDTO convert(final User user) {		
-		UserDTO userDTO = new UserDTO(user.getId(), user.getUsername());
-		userDTO.getReservations().addAll(
-				user.getReservations().stream().map((resa) -> convert(resa)).collect(Collectors.toList())
-				);
-		return userDTO;
-	}
-	
-	private ReservationDTO convert(final Reservation reservation) {
-		BookDTO bookDTO = new BookDTO(
-				reservation.getBook().getId(),
-				reservation.getBook().getName(),
-				reservation.getBook().getDescription(),
-				reservation.getBook().getStatus());
-		ReservationDTO resaDTO = new ReservationDTO(reservation.getId(), reservation.getDateResa(), bookDTO);
-		return resaDTO;
-	}
+    if (existingUser.isPresent()) {
+      throw new RuntimeException("User already exists");
+    }
 
+    User savedUser = userRepository.save(user);
+
+    return convert(savedUser);
+  }
+
+  public Optional<UserDTO> login(final LoginRequest login) {
+    Optional<User> user = this.userRepository.findByUsernameAndPassword(
+      login.getUsername(),
+      login.getPassword()
+    );
+    UserDTO userDTO = null;
+    if (user.isPresent()) {
+      userDTO = convert(user.get());
+    }
+    return Optional.ofNullable(userDTO);
+  }
+
+  private UserDTO convert(final User user) {
+    UserDTO userDTO = new UserDTO(user.getId(), user.getUsername());
+    userDTO
+      .getReservations()
+      .addAll(
+        user
+          .getReservations()
+          .stream()
+          .map(resa -> convert(resa))
+          .collect(Collectors.toList())
+      );
+    return userDTO;
+  }
+
+  private ReservationDTO convert(final Reservation reservation) {
+    BookDTO bookDTO = new BookDTO(
+      reservation.getBook().getId(),
+      reservation.getBook().getName(),
+      reservation.getBook().getDescription(),
+      reservation.getBook().getStatus()
+    );
+    ReservationDTO resaDTO = new ReservationDTO(
+      reservation.getId(),
+      reservation.getDateResa(),
+      bookDTO
+    );
+    return resaDTO;
+  }
 }
